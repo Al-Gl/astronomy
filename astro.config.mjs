5a0d0b9b99c855d88f37f373781d6c2086f43f48
Aldin <alding12@gmail.com>
2026-01-21 14:44:05 +0100

    Final configuration for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // We removed "output: hybrid" - static is now the default and supports both!
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  })
+});
